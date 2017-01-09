7d2742273a85bc6591e6e063e594e3e60d58a058
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-01-09 23:51:07 +0100

    Added a sad path

--- oauth_test.go
+++ oauth_test.go
@@ -6,22 +6,40 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var sampleToken = "my-cool-token"
+var sampleToken = "my-oauth-token"
 
 func TestOAuthSpec(t *testing.T) {
 
-        Convey("Given a HTTP request with a sample token", t, func() {
+        Convey("Given a HTTP request with a valid sample token", t, func() {
                 httpReq := http.Request{
                         Header: map[string][]string{
                                 "Authorization": {"Bearer: " + sampleToken},
                         },
                 }
+
                 Convey("When the auth header is extracted", func() {
                         token := extractAuthorizationFromHeader(&httpReq)
 
                         Convey("The auth header extracted should match the sample one", func() {
+                                So(token, ShouldNotBeNil)
                                 So(token, ShouldEqual, sampleToken)
-                                So(token, ShouldEqual, sampleToken)
+                        })
+                })
+        })
+
+        Convey("Given a HTTP request missing a token", t, func() {
+                httpReq := http.Request{
+                        Header: map[string][]string{
+                                "Authorization": {""},
+                        },
+                }
+
+                Convey("When the auth header is extracted", func() {
+                        token := extractAuthorizationFromHeader(&httpReq)
+
+                        Convey("The auth header extracted should be empty", func() {
+                                So(token, ShouldNotBeNil)
+                                So(token, ShouldBeEmpty)
                         })
                 })
         })
